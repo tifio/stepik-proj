32145baa46fd7c647676155526907d6f52df8788
FBS0\i.filimonov <i.filimonov@fbs.com>
2022-04-19 21:50:38 +0300

    point before login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
-from .base_page import BasePage
+from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from pages.locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
