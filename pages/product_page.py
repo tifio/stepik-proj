17db9f0d96cd1e464d04895cf1ad36d76387904f
FBS0\i.filimonov <i.filimonov@fbs.com>
2022-04-21 22:02:47 +0300

    add test with param

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from pages.locators import MainPageLocators
 
@@ -5,3 +6,24 @@ class ProductPage(BasePage):
     def should_be_button_add(self):
         link = self.browser.find_element(*MainPageLocators.BUTTON_LINK)
         link.click()
+
+    def should_be_name_book(self):
+        actual_name = self.browser.find_element_by_css_selector("#messages>.alert>.alertinner>strong")
+        print(actual_name.text)
+        expected_name = self.browser.find_element_by_css_selector(".breadcrumb>li.active")
+        print(expected_name.text)
+        assert expected_name.text == actual_name.text, "Wrong name a book"
+
+
+    def should_be_cost(self):
+        actual_price = self.browser.find_element_by_css_selector("#messages>.alert:last-child>.alertinner>p>strong")
+        print(actual_price.text)
+        expected_price = self.browser.find_element_by_css_selector(".product_main>p.price_color")
+        print(expected_price.text)
+        assert expected_price.text == actual_price.text, "Wrong price"
+
+
+
+    # def should_be_button_add(self):
+    #     link = self.browser.find_element(*MainPageLocators.BUTTON_LINK)
+    #     link.click()
--- test_product_page.py
+++ test_product_page.py
@@ -1,17 +1,54 @@
 import time
 from pages.product_page import ProductPage
 from pages.base_page import BasePage
+import pytest
 
 
-def test_guest_open(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
-    # link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('promo_offer', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     pageProduct = ProductPage(browser, link)
     pageProduct.open()
     pageProduct.should_be_button_add()
     pageProduct.solve_quiz_and_get_code()
+    pageProduct.should_be_name_book()
+    pageProduct.should_be_cost()
+    time.sleep(1)
 
-    time.sleep(40)
 
+
+########################## -----первая реализация тестов на память----- ##########################
+
+# def test_guest_open(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#     pageProduct = ProductPage(browser, link)
+#     pageProduct.open()
+#     pageProduct.should_be_button_add()
+#     pageProduct.solve_quiz_and_get_code()
+#     pageProduct.should_be_name_book()
+#     pageProduct.should_be_cost()
+#
+#     time.sleep(2)
+
+
+# @pytest.mark.parametrize('promo_offer', ["0", "1", "3", "4", "5", "6", "7", "8", "9"])
+# def test_guest_can_add_product_to_basket(browser, promo_offer):
+#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+#     pageProduct = ProductPage(browser, link)
+#     pageProduct.open()
+#     pageProduct.should_be_button_add()
+#     pageProduct.solve_quiz_and_get_code()
+#     pageProduct.should_be_name_book()
+#     pageProduct.should_be_cost()
+#     time.sleep(1)
 
 
