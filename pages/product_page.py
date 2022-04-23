f509d3b28fb08a8ac16341c8c9b4aee019addcb2
FBS0\i.filimonov <i.filimonov@fbs.com>
2022-04-23 21:49:35 +0300

    fix import

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,5 @@
-from .base_page import BasePage
-from .locators import ProductPageLocators
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
